31f1e88436c03aa47d865a87769ece6bd2d2a2ef
Emiliaceb <emiceballoss24@gmail.com>
2024-08-18 18:58:14 -0300

    realice galeria imagenes de videojuegos mas vendidos

---
+++ js/galeria.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll('.img-juegos-mas-vendidos');
+const descriptionBox = document.getElementById('description-box');
+
+images.forEach(image => {
+    image.addEventListener('mouseover', (event) => {
+        const description = event.target.getAttribute('data-description');
+        descriptionBox.textContent = description;
+        descriptionBox.style.display = 'block';
+        descriptionBox.style.left = `${event.pageX}px`;
+        descriptionBox.style.top = `${event.pageY + 20}px`;
+    });
+
+    image.addEventListener('mousemove', (event) => {
+        descriptionBox.style.left = `${event.pageX}px`;
+        descriptionBox.style.top = `${event.pageY + 20}px`;
+    });
+
+    image.addEventListener('mouseout', () => {
+        descriptionBox.style.display = 'none';
+    });
+});

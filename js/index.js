a6c3ab85ad7d93eae3fe66a125dd21c2dae56f2c
Elian Miranda <elianmir@outlook.com>
2024-08-25 19:48:56 -0300

    agregada la logica para buscar juegos

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@ import { colocarJuegos,rellenarSection } from "./filtrarGenero.js";
 import { rellenarGaleria } from "./galeria.js";
 import { Juego, crearJuegos } from "./classJuegos.js";
 import { crearVariables,updateNavIndex } from "./navLogueado.js";
+import inicializarBusqueda from "./buscarJuegos.js";
 const listaJuegos=crearJuegos()
 rellenarGaleria()
 rellenarSection()
 crearVariables()
 updateNavIndex()
+inicializarBusqueda()
